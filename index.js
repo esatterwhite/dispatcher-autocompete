96656d5bccb8f0f36b9396466f9ebe5125329dd2
Eric Satterwhtie <esatterwhite@wi.rr.com>
2015-04-18 13:00:09 -0500

    update zoom level.
    a little bit closer now...

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-var path = require('path')
-var express = require('express')
-var app = express();
-var PORT = process.env.PORT || 3000
-
+var path    = require('path')
+  , express = require('express')
+  , app     = express()
+  , PORT    = process.env.PORT || 3000
+  ; 
+  
 app.use(express.static( path.join( __dirname,'static' ) ) );
 app.use(express.static( path.join( __dirname, 'bower_components') ) );
 
 app.get('/', function(req, res, next){
-	res.sendFile(path.join(__dirname,'index.html'))
-})
+	res.sendFile(path.join(__dirname,'index.html'));
+});
+
 app.listen( PORT, function( e, d ){
-	console.log('server listening on port %d', PORT)
-})
+	console.log('server listening on port %d', PORT);
+});
